a123506d09b866a26d611cff77e7a351d8806af1
Allison-Huang <allison.dexin@gmail.com>
2020-04-24 16:37:51 -0400

    Changed the style of index.html to be mobile-responsive

--- webpage/define-objects.js
+++ webpage/define-objects.js
@@ -1,11 +1,13 @@
 /* mini names */
 const h = {};
+const ho = {};
 const w = {};
 const i = {};
 const d = [];
 
 /* readable names */
 const hypernyms = h;
+const hyponyms = ho;
 const wordToIdMap = w;
 const idToWordMap = i;
 const definitions = d;
